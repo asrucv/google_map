6f5d44763b4423017e4ffc866b608ba179a9ecb0
asrucv <asrucv@gmail.com>
2023-01-20 11:08:11 +0530

    first commit

---
+++ lib/homescreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomeScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+   
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:google_map/homescreen.dart';
+
+void main() => runApp(const MaterialApp(home: HomeScreen(),));
+
